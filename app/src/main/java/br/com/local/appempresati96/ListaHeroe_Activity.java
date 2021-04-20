e3fbe0a63ba51522d83716a5dd696fff5acff9f9
Laercio-Note <lndsilva@hotmail.com>
2021-04-20 15:41:38 -0300

    cardView e ratingbar

---
+++ app/src/main/java/br/com/local/appempresati96/ListaHeroe_Activity.java
@@ -0,0 +1,14 @@
+package br.com.local.appempresati96;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaHeroe_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lista_heroe_layout);
+    }
+}
